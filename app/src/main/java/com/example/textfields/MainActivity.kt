81e280baaf5f194ddb2fc6eab18ab20a100acc37
Михаил <mishavolkov20150296@gmail.com>
2023-03-30 22:09:02 +0300

    Подправил

--- app/src/main/java/com/example/textfields/MainActivity.kt
+++ app/src/main/java/com/example/textfields/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.textfields
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
+import android.view.MenuItem
+import android.view.View
+import android.widget.PopupMenu
+import androidx.annotation.MenuRes
 import com.example.textfields.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private var binding: ActivityMainBinding? = null
 
@@ -23,5 +27,38 @@ class MainActivity : AppCompatActivity() {
             return@setOnKeyListener true
         }
 
+        binding?.popup?.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        showMenu(view,R.menu.popup_menu)
+    }
+    //@MenuRes
+    private fun showMenu(view: View, menuRes: Int){
+        val popup = PopupMenu(this,view)
+        popup.menuInflater.inflate(menuRes,popup.menu)
+
+        popup.setOnMenuItemClickListener {  menuItem: MenuItem ->
+            when (menuItem.itemId){
+                R.id.catalogMenuPopup -> {
+                    binding?.selectedItems?.text = getString(R.string.catalog)
+                    true
+                }
+                R.id.favoritMenuPopup -> {
+                    binding?.selectedItems?.text = getString(R.string.favorit)
+                    true
+                }
+                R.id.settingMenuPopup -> {
+                    binding?.selectedItems?.text = getString(R.string.setting)
+                    true
+                }
+                else -> true
+            }
+        }
+        popup.setOnDismissListener {
+            binding?.condition?.text = getString(R.string.closed)
+        }
+        popup.show()
     }
 }
